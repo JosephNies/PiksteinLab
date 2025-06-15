dabc67645b35d9d450cb6643103be3f13b04aed9
Joseph Nies <boardrfolife@gmail.com>
2025-06-15 11:41:17 -0700

    branding removal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
-/*
-	Future Imperfect by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+// Main JS file for the website, handling responsive design, menu, search functionality, and intro section placement.
+// This file is part of a responsive web design project.
 
 (function($) {
 
