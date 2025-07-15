c5ef23d28d8d022989e6439d60d91816d7c355b3
Joseph Nies <boardrfolife@gmail.com>
2025-07-15 00:11:04 -0700

    full-site-deploy v2

--- assets/js/headerfooter.js
+++ assets/js/headerfooter.js
@@ -1,26 +1,120 @@
 class MyHeader extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
-					<header id="header">
-						<h1><a href="index.html">Pikstein</a></h1>
-						<nav class="links">
-							<ul>
-								<li><a href="http://piksteinlab.org/donate/">Donate</a></li>
-								<li><a href="http://piksteinlab.org/pikstein-lab/">Pikstein Lab</a></li>
-								<li><a href="http://piksteinlab.org/about/">About</a></li>
-								<li><a href="https://piksteinlab.org/events/">Events</a></li>
-							</ul>
-						</nav>
-						<nav class="main">
-							<ul>
-								<li class="menu">
-									<a class="fa-bars" href="#menu">Menu</a>
-								</li>
-							</ul>
-						</nav>
-					</header>
-					
-        `;
-    }
-} 
-customElements.define('header-component', MyHeader);
+  connectedCallback() {
+    this.innerHTML = `
+		<header id="header">
+			<h1><a href="index.html">Pikstein</a></h1>
+			<nav class="links">
+				<ul>
+					<li><a href="donate.html">Donate</a></li>
+					<li><a href="pikstein-lab.html">Pikstein Lab</a></li>
+					<li><a href="about.html">About</a></li>
+					<li><a href="events.html">Events</a></li>
+				</ul>
+			</nav>
+			<nav class="main">
+				<ul>
+					<li class="menu">
+						<a class="fa-bars" href="#menu">Menu</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	`;
+  }
+}
+customElements.define("header-component", MyHeader);
+
+class MyHamburger extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+          <ul class="links">
+            <li>
+              <a href="donate.html">
+                <h3>Donate</h3>
+              </a>
+            </li>
+            <li>
+              <a href="pikstein-lab.html">
+                <h3>Pikstein Lab</h3>
+              </a>
+            </li>
+            <li>
+              <a href="about.html">
+                <h3>About</h3>
+              </a>
+            </li>
+            <li>
+              <a href="events.html">
+                <h3>Events</h3>
+              </a>
+            </li>
+          </ul>
+	`;
+  }
+}
+customElements.define("hamburger-component", MyHamburger);
+
+class MyFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <ul class="icons">
+            <li>
+              <a
+                href="https://www.facebook.com/rachel.pikstein"
+                class="icon brands fa-facebook-f"
+                target="_blank"
+                ><span class="label">Facebook</span></a
+              >
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/pikstein_lab/"
+                class="icon brands fa-instagram"
+                target="_blank"
+                ><span class="label">Instagram</span></a
+              >
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/@PiksteinLab"
+                class="icon brands fa-youtube"
+				target="_blank"
+                ><span class="label">YouTube</span></a
+              >
+            </li>
+            <li>
+              <a
+                href="mailto:rpbiologist+website@gmail.com"
+                class="icon solid fa-envelope"
+                ><span class="label">Email</span></a
+              >
+            </li>
+        </ul>
+		<p class="copyright">
+			&copy;
+			<script>
+				document.write(new Date().getFullYear());
+			</script>
+			<a href="http://rachelpikstein.com/"  target="_blank">Rachel Pikstein</a>. All rights
+			reserved.
+		</p>        
+	`;
+  }
+}
+customElements.define("footer-component", MyFooter);
+
+class MyAbout extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+		<h2>About</h2>
+          <p>
+            Discovering the wonders of biology through the lens of
+			curiosity and innovation. Join us in exploring.
+          </p>
+          <ul class="actions">
+            <li><a href="about.html" class="button">Learn More</a></li>
+          </ul>
+	`;
+  }
+}
+customElements.define("about-component", MyAbout);
