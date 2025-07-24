4ede8d184ba9fe9f9dac6b64ead9f451709ef9e4
Joseph Nies <boardrfolife@gmail.com>
2025-07-23 19:15:12 -0700

    gtm script

---
+++ assets/js/analytics.js
@@ -0,0 +1,20 @@
+// Header no submenus
+class GTMHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+		<script>
+      (function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, 'script', 'dataLayer', 'GTM-TVPK6975');
+    </script>
+	`;
+  }
+}
+customElements.define('gtm-header', GTMHeader);
