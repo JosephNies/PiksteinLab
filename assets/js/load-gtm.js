b33ed1d16dcde34ae038d74f07dea98d26769420
Joseph Nies <boardrfolife@gmail.com>
2025-07-31 00:15:19 -0700

    cookie consent update

--- assets/js/load-gtm.js
+++ assets/js/load-gtm.js
@@ -1,5 +1,21 @@
-// Expose a function so GTM can be loaded after the visitor gives consent.
-// Call `window.loadGoogleTagManager('GTM-XXXXXXX')` once cookies are accepted.
+// Load GTM after a short delay to allow consent banner to be seen
+setTimeout(function() {
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+
+    j.async = true;
+    j.id = 'gtm-script';
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-TVPK6975');
+}, 2000); // 2 second delay
+
+// Keep the function for backward compatibility
 window.loadGoogleTagManager = function (id) {
   if (!id) return;
 
